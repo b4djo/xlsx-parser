c14a78ed651fb4da0f8d6b18f4ae1ee2168f145f
Robert <robert@eljur.ru>
2019-01-24 18:01:07 +0300

    <master> Go to peewee

--- xlsx-parser.py
+++ xlsx-parser.py
@@ -2,24 +2,17 @@ from openpyxl import load_workbook
 
 wb = load_workbook('')
 
-sheet = wb.get_sheet_by_name('Лист1')
+sheet = wb.get_sheet_by_name('')
 
-import MySQLdb
-db = MySQLdb.connect(host="localhost",
-                     user="root",
-                     passwd="root",
-                     db="test",
-                     charset='utf8',
-                     use_unicode=True)
-cur = db.cursor()
+import peewee
+from peewee import *
+
+db = MySQLDatabase('test', user='root', password='root', host='localhost', port=3316)
+Data1 = Table('table1', ('id', 'name'))
+Data1 = Data1.bind(db)
+
 maxCountRow = sheet.max_row
 
 for i in range(2, maxCountRow):
     value = sheet['C' + str(i)].value
-    sql = """INSERT INTO table1 (name)
-            VALUES ('%(value)s')
-            """ % {"value": value}
-    cur.execute(sql)
-
-db.commit()
-db.close()
+    Data1.insert(name=value).execute()
